00998fa8131598c3ccf5c8a83e3f0236d1d2c1be
K1myush1n <kys010306@sogang.ac.kr>
2022-05-10 17:58:22 +0900

    20220510

---
+++ chapter02/IdSearchSeq1.cpp
@@ -0,0 +1,15 @@
+#include <tchar.h>
+#include <iostream>
+using namespace std;
+
+int nData(20);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    int nData(10);
+
+    cout << nData << endl;
+    cout << argc << endl;
+
+    return 0;
+}
